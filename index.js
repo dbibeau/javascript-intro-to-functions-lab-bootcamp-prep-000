ea2c412116f833d2283ef50de834bcf997f30b5c
daniel bibeau <dbibeau85@gmail.com>
2017-11-15 16:45:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,17 @@ function shout(string){
  function logwhisper(string){
    console.log(string.toLowerCase())
  }
- 
+ function sayHiToGrandma(string){
+   if (string.toLowercase() === string) {
+     return "I can't hear you!"
+   }
+if (string.toUpperCase() === string) {
+  return "YES INDEED!"
+}
+   if(string === "I love you, Grandma."){
+     return "I love you, too"
+   }
+   
+   
+ }
  
